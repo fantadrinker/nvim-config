69bf5aa64f557e810d89706abb6833f1ea454d8b
fantadrinker <yizhi.tsui@gmail.com>
2023-07-03 15:38:32 -0700

    init

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,25 @@
+
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+  lsp.buffer_autoformat()
+end)
+
+lsp.setup()
+
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = {
+    ['<Cr>'] = cmp.mapping.confirm({select = true,}),
+    ['<S-Tab>'] = vim.NIL,
+    ['<Tab>'] = vim.NIL,
+  },
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'buffer', keyword_length = 3},
+    {name = 'luasnip', keyword_length = 2},
+  }
+})
---
+++ lua/fredtsui/init.lua
@@ -0,0 +1,2 @@
+require("fredtsui.remap")
+require("fredtsui.set")
